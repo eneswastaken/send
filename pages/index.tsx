df6eda477e671cdacb8980a5604cc52f56cae3d4
Enes <120328716+eneswastaken@users.noreply.github.com>
2024-05-07 20:07:40 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>💬 send.cnrad.dev</title>
+                <title>💬 send.ensomg.net.tr</title>
             </Head>
 
             <Background src="/background.jpg" />
